b1e33672576fd1ff514c183efcbe5c9ce8d80833
mplevako <mplevako@gmail.com>
2015-09-23 21:31:31 +0300

    Implement nondiscriminating layout counter
    The counter treats pieces of the same kind as indistinguishable and
    does not count layouts that only differ in permutations of such
    pieces as distinct.

--- src/main/scala/layout/LayoutCounter.scala
+++ src/main/scala/layout/LayoutCounter.scala
@@ -7,23 +7,20 @@ trait LayoutCounter {
   type Layout = Set[Piece]
   type PieceStock = Seq[Piece#Cell => Piece]
 
-  def countLayouts(boardRows: Int, boardColumns: Int)(pieces: PieceStock): Long = {
+  def countLayouts(boardRows: Int, boardColumns: Int)(pieceStock: PieceStock): Long = {
     def notTakenBy(piece: Piece) = (cell: Piece#Cell) => !(piece controls cell)
 
-    def countLayouts(openCells: Set[Piece#Cell], placedPieces: Layout, pieceStock: PieceStock): Long = pieceStock match {
-      case Nil if placedPieces.isEmpty => 0L
-      case Nil => 1L
+    def enumerateLayouts(openCells: Set[Piece#Cell], placedPieces: Layout, pieceStock: PieceStock): Set[Set[Piece]] = pieceStock match {
+      case Nil => if(placedPieces.isEmpty) Set.empty else Set(placedPieces)
       case pieceAt::stock =>
-        var layoutNumber = 0L
         for {
-          cell <- openCells
-          currentPiece = pieceAt(cell) if placedPieces map (_.cell) forall notTakenBy(currentPiece)
-        } layoutNumber += countLayouts((openCells - cell) filter notTakenBy(currentPiece),
-                                       placedPieces + currentPiece, stock)
-
-        layoutNumber
+              cell <- openCells
+              currentPiece = pieceAt(cell) if placedPieces map (_.cell) forall notTakenBy(currentPiece)
+              layouts <- enumerateLayouts((openCells - cell) filter notTakenBy(currentPiece),
+                                          placedPieces + currentPiece, stock)
+            } yield layouts
     }
 
-    countLayouts((for(r <- 0 until boardRows; c <- 0 until boardColumns) yield (r,c)).toSet, Set.empty, pieces)
+    enumerateLayouts((for(r <- 0 until boardRows; c <- 0 until boardColumns) yield (r,c)).toSet, Set.empty, pieceStock).size
   }
 }
